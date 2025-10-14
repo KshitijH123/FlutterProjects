efc1a3f32f10e103f962c22d9ab3fe90fb2de2b1
KshitijH123 <kshitijhapase@gmail.com>
2025-10-14 23:22:41 +0530

    login ui and dependencies

--- login_firebase/lib/login_screen.dart
+++ login_firebase/lib/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:login_firebase/register_user_screen.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -8,15 +9,70 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  void login() {}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Login Screen'),centerTitle: true,elevation: 2),
+      appBar: AppBar(
+        backgroundColor: Colors.blue[300],
+        title: Text(
+          'Login Screen',
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.w500),
+        ),
+        centerTitle: true,
+      ),
       body: Column(
         children: [
-          TextField(
-            decoration: InputDecoration(border: OutlineInputBorder()),
+          SizedBox(height: 24),
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: TextField(
+              controller: emailController,
+              decoration: InputDecoration(
+                hintText: 'Email',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(16),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: TextField(
+              controller: passwordController,
+              decoration: InputDecoration(
+                hintText: 'Password',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(16),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 8),
+          ElevatedButton(onPressed: login, 
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.blue[300],
+            foregroundColor: Colors.white,
+            minimumSize: Size(250, 50),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(16)
+            )
           )
+          ,child: Text('Login',style: TextStyle(fontSize: 18),)),
+          SizedBox(height: 8),
+          TextButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => RegisterUserScreen()),
+              );
+            },
+            child: Text('Register User',style: TextStyle(color: Colors.blue[300])),
+          ),
         ],
       ),
     );
---
+++ login_firebase/lib/register_user_screen.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+class RegisterUserScreen extends StatefulWidget {
+  const RegisterUserScreen({super.key});
+
+  @override
+  State<RegisterUserScreen> createState() => _RegisterUserScreenState();
+}
+
+class _RegisterUserScreenState extends State<RegisterUserScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue[300],
+        title: Text(
+          'Register User',
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.w500),
+        ),
+        centerTitle: true,
+        elevation: 2,
+      ),
+      body: Column(
+        children: [
+          SizedBox(height: 28),
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: 'Email',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(16),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: 'password',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(16),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: 'Confirm password',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(16),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 32),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.blue[300],
+              foregroundColor: Colors.white,
+              minimumSize: Size(250, 50),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16),
+              ),
+            ),
+            onPressed: () {},
+            child: Text('Register User',style: TextStyle(fontSize: 18),),
+          ),
+        ],
+      ),
+    );
+  }
+}
